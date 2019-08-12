e9c1f10275099cdef732893fd9b2d944adb2ab5b
Felipe Malara <malarahfelipe@gmail.com>
2019-08-12 11:14:34 -0300

    Alterações para terminal Felipe Malara

--- themes/bobby/bobby.theme.bash
+++ themes/bobby/bobby.theme.bash
@@ -1,29 +1,25 @@
 #!/usr/bin/env bash
 
-SCM_THEME_PROMPT_DIRTY=" ${red}✗"
+SCM_THEME_PROMPT_DIRTY=" ${red}🔥️"
 SCM_THEME_PROMPT_CLEAN=" ${bold_green}✓"
 SCM_THEME_PROMPT_PREFIX=" ${green}|"
 SCM_THEME_PROMPT_SUFFIX="${green}|"
 
-GIT_THEME_PROMPT_DIRTY=" ${red}✗"
+GIT_THEME_PROMPT_DIRTY=" ${red}🔥️"
 GIT_THEME_PROMPT_CLEAN=" ${bold_green}✓"
-GIT_THEME_PROMPT_PREFIX=" ${green}|"
-GIT_THEME_PROMPT_SUFFIX="${green}|"
+GIT_THEME_PROMPT_PREFIX=" ${bold_cyan}"$(git config --global user.name)" ${green}| "
+GIT_THEME_PROMPT_SUFFIX="${green} |"
 
 RVM_THEME_PROMPT_PREFIX="|"
 RVM_THEME_PROMPT_SUFFIX="|"
 
 __bobby_clock() {
-  printf "$(clock_prompt) "
-
-  if [ "${THEME_SHOW_CLOCK_CHAR}" == "true" ]; then
-    printf "$(clock_char) "
-  fi
+  printf "$(clock_prompt) 🤘"
 }
 
 function prompt_command() {
     #PS1="${bold_cyan}$(scm_char)${green}$(scm_prompt_info)${purple}$(ruby_version_prompt) ${yellow}\h ${reset_color}in ${green}\w ${reset_color}\n${green}→${reset_color} "
-    PS1="\n$(battery_char) $(__bobby_clock)${yellow}$(ruby_version_prompt) ${purple}\h ${reset_color}in ${green}\w\n${bold_cyan}$(scm_prompt_char_info) ${green}→${reset_color} "
+    PS1="\n$(__bobby_clock)${yellow}$(ruby_version_prompt) ${purple}\h ${reset_color}in ${green}\w\n${bold_cyan}$(scm_prompt_char_info) ${green}→${reset_color} "
 }
 
 THEME_SHOW_CLOCK_CHAR=${THEME_SHOW_CLOCK_CHAR:-"true"}
